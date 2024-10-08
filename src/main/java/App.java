943fdcf3ebb37649da3e17fd88f918d20bf687e4
Andrey <dudinend@gmail.com>
2024-10-08 20:02:47 +0400

    Cat++

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2==bean? "Они один объект":"это разные объекты");
+        Cat cat1=(Cat)applicationContext.getBean("cat");
+        Cat cat2=(Cat)applicationContext.getBean("cat");
+        System.out.println(cat1==cat2? "Они один объект":"это разные объекты");
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.Bean;
+
+public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
